6ff3900af2126fb91d9310776f6f8cf5cdcd1695
Mikael Andri Ingason <33830730+MikaelAndriIngason@users.noreply.github.com>
2023-09-07 18:13:57 +0000

    Initial commit

---
+++ src/main/java/is/hi/hbv501g/hbv501gteam4/Hbv501GTeam4Application.java
@@ -0,0 +1,13 @@
+package is.hi.hbv501g.hbv501gteam4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hbv501GTeam4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hbv501GTeam4Application.class, args);
+	}
+
+}
---
+++ src/test/java/is/hi/hbv501g/hbv501gteam4/Hbv501GTeam4ApplicationTests.java
@@ -0,0 +1,13 @@
+package is.hi.hbv501g.hbv501gteam4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hbv501GTeam4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
