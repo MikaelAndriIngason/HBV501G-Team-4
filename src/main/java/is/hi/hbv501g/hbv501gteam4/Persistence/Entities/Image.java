6b7948d66c26dcb0057c82cf2ef6ad46ff888dbd
Hildur Bjarkadottir <hib24@hi.is>
2023-10-13 17:14:13 +0200

    images to disc, add, update and delete

---
+++ src/main/java/is/hi/hbv501g/hbv501gteam4/Persistence/Entities/Image.java
@@ -0,0 +1,41 @@
+package is.hi.hbv501g.hbv501gteam4.Persistence.Entities;
+
+import jakarta.persistence.*;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@Entity
+@Table(name = "Images")
+public class Image {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long imageID;
+
+    private String image;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Disc disc;
+
+    public Image(){}
+
+    public Image(String image, Disc disc) {
+        this.image = image;
+        this.disc = disc;
+    }
+
+    public long getImageID() {
+        return imageID;
+    }
+
+    public void setImageID(long imageID) {
+        this.imageID = imageID;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+}
